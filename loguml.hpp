404c871ccc10ea2b6fed7d5bb3fdfd9674ae7222
Julian Teh <mail@tehj.io>
2023-06-03 23:35:54 +0800

    Adding a test node class to test.cpp and updating the graphviz algorithm to output a value.
    Algorithm is still wrong, this submission is just to give us a better
    starting point so we can see the values that are being output

--- loguml.hpp
+++ loguml.hpp
@@ -12,38 +12,40 @@
 // };
 // We assume the DAG is made up of Node objects, which internally hold all their relations
 
-template<typename Node, typename InfoSet>
+template<typename Node>
 void generateGraphvizForDAG(
     const std::unordered_map<std::string, Node>& dag, 
-    const std::string &node, // Starting node ID
-    const std::function<std::vector<std::string>>(Node)> &visitor, // Extract the info to print from each Node
-    std::ostream& out
+    const std::string &nodeId, // Starting node ID
+    const std::function<std::vector<std::string>(const Node&)> &visitor, // Extract the info to print from each Node
+    std::ostream& oss
     )
 {
-    out << "digraph {\n";
+    oss << "digraph {\n";
 
     // Set of visited ID's
     std::set<std::string> visited = { };
 
     // Set of next to visit ID's
-    std::set<std::string> toVisit = { start };
+    std::set<std::string> toVisit = { nodeId };
 
     while (!toVisit.empty()) {
       // Starting node
-      const Node start = dag[node];
+      const Node currNode = dag.at(nodeId);
 
-      for (const auto & info : visitor(start)) {
-        out << start.GetID() << "[label=\"" info << "\"];\n";
+      // Print the labels associated with this node into the node
+      for (const auto & info : visitor(currNode)) {
+        oss << currNode.GetID() << "[label=\"" << info << "\"];\n";
       }
 
-      for (const auto & child : start.GetChildren() {
-        out << start.GetID() << " -> " << child.GetID() << std::endl;
-        toVisit.insert(child.GetID());
+      // Generate the links to the next node
+      for (const auto & childId : currNode.GetChildren()) {
+        oss << currNode.GetID() << " -> " << childId << std::endl;
+        toVisit.insert(childId);
       }
 
-      visited.insert(start.GetID());
-      toVisit.remove(start.GetID());
+      visited.insert(currNode.GetID());
+      toVisit.erase(currNode.GetID());
     }
 
-    out << "}\n";
+    oss << "}\n";
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,60 @@
 #include "../loguml.hpp"
 #include <gtest/gtest.h>
+#include <iostream>
+
+
+class TestNode {
+  public: 
+    TestNode() : m_TestId(std::to_string(s_TestId++)) {
+      
+    }
+
+    auto GetID() const -> std::string {
+      return m_TestId;
+    }
+
+    auto GetChildren() const -> std::vector<std::string> {
+      return m_Children;
+    }
+
+    void AddChild(std::string childId) {
+      if ( std::find(m_Children.begin(), m_Children.end(), childId) != m_Children.end() ) {
+        m_Children.push_back(childId);
+      }
+    }
+
+  private:
+    static int s_TestId;
+
+    std::string m_TestId;
+    std::vector<std::string> m_Children;
+};
+
+int TestNode::s_TestId = 0;
+
+auto visitWriteId(const TestNode& node) -> std::vector<std::string> {
+  return std::vector<std::string>{node.GetID()};
+}
 
 TEST(TestGTest, BasicAsserts) {
-  EXPECT_STRNE("This", "That");
+  auto node1 = TestNode();
+  auto node2 = TestNode();
+  auto node3 = TestNode();
 
-  EXPECT_EQ(10, 5 * 2);
+  node1.AddChild(node2.GetID());
+  node1.AddChild(node3.GetID());
+
+  std::unordered_map<std::string, TestNode> dag = {
+    {node1.GetID(), node1},
+    {node2.GetID(), node2},
+    {node3.GetID(), node3}
+  };
+  
+  generateGraphvizForDAG<TestNode>(
+      dag, 
+      node1.GetID(), 
+      visitWriteId, 
+      std::cout);
+
+  ASSERT_EQ(0, 1);
 }
